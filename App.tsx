08843b04352bd47a43cfc3efe98a89f9126d41c3
odair-pedroso <odairpedroso90@gmail.com>
2022-04-30 16:33:04 -0300

    add

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bem vindo Odair!!</Text>
+      <Text>Bem vindo Odair!!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
